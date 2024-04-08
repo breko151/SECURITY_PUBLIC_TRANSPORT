df4e349a3e644405f64e2c3a299cb949e2ad2a29
Pablo Suárez Pérez <brekopablo@gmail.com>
2024-04-08 00:23:56 -0600

    Puntos dentro mapa

--- app.py
+++ app.py
@@ -12,12 +12,55 @@ def init_map(center=(19.4325019109759, -99.1322510732777), zoom_start=10, map_ty
     return folium.Map(location=center, zoom_start=zoom_start, tiles=map_type)
 
 
+# DataImages for stations.
+POINTSM = {
+    'L1': './images/circulos/STCMetro_L1.png',
+    'L2': './images/circulos/STCMetro_L2.png',
+    'L3': './images/circulos/STCMetro_L3.png',
+    'L4': './images/circulos/STCMetro_L4.png',
+    'L5': './images/circulos/STCMetro_L5.png',
+    'L6': './images/circulos/STCMetro_L6.png',
+    'L7': './images/circulos/STCMetro_L7.png',
+    'L8': './images/circulos/STCMetro_L8.png',
+    'L9': './images/circulos/STCMetro_L9.png',
+    'LA': './images/circulos/STCMetro_LA.png',
+    'LB': './images/circulos/STCMetro_LB.png',
+    'L12': './images/circulos/STCMetro_L12.png',
+}
+
+
+POINTSMB = {
+    'L1': './images/circulos/MB_L1.png',
+    'L2': './images/circulos/MB_L2.png',
+    'L3': './images/circulos/MB_L3.png',
+    'L4': './images/circulos/MB_L4.png',
+    'L5': './images/circulos/MB_L5.png',
+    'L6': './images/circulos/MB_L6.png',
+    'L7': './images/circulos/MB_L7.png',
+}
+
+
 # Plot map.
-def plot_from_df(df, folium_map):
+def plot_from_df(df, folium_map, DICT_COLORS):
     for i, row in df.iterrows():
+        icon = folium.CustomIcon(
+            DICT_COLORS[row.linea],
+            icon_size=(15, 15)
+        )
         folium.Marker([row.latitud, row.longitud],
-                      tooltip=f'{row.cve_est}: {row.linea}, {row.nombre}').add_to(folium_map)
+                      icon=icon,
+                    tooltip=f'{row.cve_est}: {row.nombre}, Línea: {row.linea[1:]}').add_to(folium_map)
     return folium_map
+
+
+# Get columns from cve_est.
+def get_station(df, cve_est, column=None):
+    if column is None:
+        result = df[df['cve_est'] == cve_est]
+    else:
+        filter = df_stations_metro[df_stations_metro['cve_est'] == cve_est]
+        result = filter[column].to_list()[0]
+    return result
 
 
 # Page config.
@@ -25,8 +68,7 @@ st.set_page_config(page_title="Página Principal",
     initial_sidebar_state="expanded",
     layout="wide",
     page_icon="🚈")
-# Hide the legend of "Made with streamlit" and hamburger menu
-# https://discuss.streamlit.io/t/remove-made-with-streamlit-from-bottom-of-app/1370/2
+# Hide the legend of "Made with streamlit" and hamburger menu.
 hide_streamlit_style = """
             <style>
             #MainMenu {visibility: hidden;}
@@ -67,24 +109,28 @@ def metro():
         col1, col2 = st.columns(2)
         with col1:
             m = init_map()
-            m = plot_from_df(df_stations_metro, m)
+            m = plot_from_df(df_stations_metro, m, POINTSM)
             level1_map_data = st_folium(m)
             st.session_state.selected_id = level1_map_data['last_object_clicked_tooltip']
         if 'selected_id' not in st.session_state:
-            col2.title("NOMBRE ESTACIÓN")
+            col2.title("ES NECESARIO SELECCIONAR UNA ESTACIÓN")
+            col2.subheader("AL SELECCIONAR APARECERÁ LA SIGUIENTE INFORMACIÓN:")
             col2.write("TOP DELITOS")
             col2.write("COMPARACIÓN DE GÉNEROS")
             col2.write("EDAD")
             col2.write("DISTANCIAS DE LOS DELITOS")
         else:
             if st.session_state.selected_id is not None:
-                col2.title(f'{st.session_state.selected_id}')
+                cve_est = st.session_state.selected_id.split(":")[0]
+                name_est = get_station(df_stations_metro, cve_est, "nombre")
+                col2.title(f'{name_est}')
                 col2.write("TOP DELITOS")
                 col2.write("COMPARACIÓN DE GÉNEROS")
                 col2.write("EDAD")
                 col2.write("DISTANCIAS DE LOS DELITOS")
             else:
-                col2.title("NOMBRE ESTACIÓN")
+                col2.title("ES NECESARIO SELECCIONAR UNA ESTACIÓN")
+                col2.subheader("AL SELECCIONAR APARECERÁ LA SIGUIENTE INFORMACIÓN:")
                 col2.write("TOP DELITOS")
                 col2.write("COMPARACIÓN DE GÉNEROS")
                 col2.write("EDAD")
